91842db1d99cd0e651ebb761a8c7e95e0ac0306c
Anwesh43 <anweshthecool0@gmail.com>
2019-05-28 03:10:49 +0530

    adding InToTangentCircle class

--- intotangentcircleview/src/main/java/com/anwesh/uiprojects/intotangentcircleview/InToTangentCircleView.kt
+++ intotangentcircleview/src/main/java/com/anwesh/uiprojects/intotangentcircleview/InToTangentCircleView.kt
@@ -168,7 +168,31 @@ class InToTangentCircleView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class InToTangentCircle(var i : Int) {
+
+        private val root : ITTCNode = ITTCNode(0)
+        private var curr : ITTCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
